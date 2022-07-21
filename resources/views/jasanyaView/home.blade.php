290118f7d977a715b9b2a18a0923606c67008c4f
iban rama <syaiban95@gmail.com>
2022-07-22 04:09:09 +0700

    ngubah lorem pada jasa kami

--- resources/views/jasanyaView/home.blade.php
+++ resources/views/jasanyaView/home.blade.php
@@ -41,6 +41,7 @@
 <!-- Navbar & Hero End -->
 
 
+{{--
 <!-- Feature Start -->
 <div class="container-xxl">
     <div class="container py-5 px-lg-5">
@@ -72,7 +73,7 @@
         </div>
     </div>
 </div>
-<!-- Feature End -->
+<!-- Feature End --> --}}
 
 
 <!-- About Start -->
@@ -126,6 +127,7 @@
 <!-- About End -->
 
 
+{{--
 <!-- Facts Start -->
 <div class="container-xxl bg-primary fact py-5 wow fadeInUp" data-wow-delay="0.1s">
     <div class="container py-5 px-lg-5">
@@ -153,16 +155,16 @@
         </div>
     </div>
 </div>
-<!-- Facts End -->
+<!-- Facts End --> --}}
 
 
 <!-- Service Start -->
 <div class="container-xxl" id="service">
     <div class="container py-5 px-lg-5">
         <div class="wow fadeInUp" data-wow-delay="0.1s">
-            <p class="section-title text-secondary justify-content-center"><span></span>Our Services<span></span>
-            </p>
-            <h1 class="text-center mb-5">What Solutions We Provide</h1>
+            {{-- <p class="section-title text-secondary justify-content-center"><span></span>Our Services<span></span>
+            </p> --}}
+            <h1 class="text-center mb-5">Jasa Kami</h1>
         </div>
         <div class="row g-4">
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
@@ -170,10 +172,11 @@
                     <div class="service-icon flex-shrink-0 ">
                         <i class="fa fa-search fa-2x"></i>
                     </div>
-                    <h5 class="mb-3">SEO Optimization</h5>
-                    <p class="m-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet
-                        diam sed stet lorem.</p>
-                    <a class="btn btn-square" href=""><i class="fa fa-arrow-right"></i></a>
+                    <h5 class="mb-3">Website</h5>
+                    <p class="m-0">jasa pembuatan website</p>
+                    <a class="btn btn-square"
+                        href="https://wa.me/6281290853613?text=Halo%20admin%20jasanya,%20jasa%20pembuatan%20website"
+                        target="blank"><i class="fa fa-arrow-right"></i></a>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
@@ -182,9 +185,10 @@
                         <i class="fa fa-laptop-code fa-2x"></i>
                     </div>
                     <h5 class="mb-3">Web Design</h5>
-                    <p class="m-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet
-                        diam sed stet lorem.</p>
-                    <a class="btn btn-square" href=""><i class="fa fa-arrow-right"></i></a>
+                    <p class="m-0">jasa pembuatan desaign website</p>
+                    <a class="btn btn-square"
+                        href="https://wa.me/6281290853613?text=Halo%20admin%20jasanya,%20jasa%20pembuatan%20desaign%20website"
+                        target="blank"><i class="fa fa-arrow-right"></i></a>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
@@ -192,10 +196,11 @@
                     <div class="service-icon flex-shrink-0">
                         <i class="fab fa-facebook-f fa-2x"></i>
                     </div>
-                    <h5 class="mb-3">Social Media Marketing</h5>
-                    <p class="m-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet
-                        diam sed stet lorem.</p>
-                    <a class="btn btn-square" href=""><i class="fa fa-arrow-right"></i></a>
+                    <h5 class="mb-3">Aplikasi Mobile</h5>
+                    <p class="m-0">jasa pembuatan aplikasi mobile</p>
+                    <a class="btn btn-square"
+                        href="https://wa.me/6281290853613?text=Halo%20admin%20jasanya,%20jasa%20pembuatan%20aplikasi%20mobile"
+                        target="blank"><i class="fa fa-arrow-right"></i></a>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
@@ -203,40 +208,21 @@
                     <div class="service-icon flex-shrink-0">
                         <i class="fa fa-mail-bulk fa-2x"></i>
                     </div>
-                    <h5 class="mb-3">Email Marketing</h5>
-                    <p class="m-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet
-                        diam sed stet lorem.</p>
-                    <a class="btn btn-square" href=""><i class="fa fa-arrow-right"></i></a>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                <div class="service-item d-flex flex-column text-center rounded">
-                    <div class="service-icon flex-shrink-0">
-                        <i class="fa fa-thumbs-up fa-2x"></i>
-                    </div>
-                    <h5 class="mb-3">PPC Advertising</h5>
-                    <p class="m-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet
-                        diam sed stet lorem.</p>
-                    <a class="btn btn-square" href=""><i class="fa fa-arrow-right"></i></a>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                <div class="service-item d-flex flex-column text-center rounded">
-                    <div class="service-icon flex-shrink-0">
-                        <i class="fab fa-android fa-2x"></i>
-                    </div>
-                    <h5 class="mb-3">App Development</h5>
-                    <p class="m-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet
-                        diam sed stet lorem.</p>
-                    <a class="btn btn-square" href=""><i class="fa fa-arrow-right"></i></a>
+                    <h5 class="mb-3">API</h5>
+                    <p class="m-0">jasa pembuatan API</p>
+                    <a class="btn btn-square"
+                        href="https://wa.me/6281290853613?text=Halo%20admin%20jasanya,%20jasa%20pembuatan%20API"
+                        target="blank"><i class="fa fa-arrow-right"></i></a>
                 </div>
             </div>
         </div>
     </div>
 </div>
+</div>
 <!-- Service End -->
 
 
+{{--
 <!-- Newsletter Start -->
 <div class="container-xxl bg-primary newsletter wow fadeInUp" data-wow-delay="0.1s">
     <div class="container py-5 px-lg-5">
@@ -256,7 +242,7 @@
         </div>
     </div>
 </div>
-<!-- Newsletter End -->
+<!-- Newsletter End --> --}}
 
 
 <!-- Projects Start -->
