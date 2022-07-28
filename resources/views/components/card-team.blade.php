ff74a77d3d0e6a523e09f48f4e8352defa57bbd3
CROWNIX <rahmatfauzi841@gmail.com>
2022-07-28 20:38:14 +0700

    Merubah foto seperti telur menjadi lingkaran

--- resources/views/components/card-team.blade.php
+++ resources/views/components/card-team.blade.php
@@ -1,7 +1,7 @@
 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
     <div class="shadow hover-scale-1 team-item bg-light rounded">
         <div class="text-center border-bottom p-4">
-            <img class="img-fluid rounded-circle mb-4" src="{{ $foto }}" alt="" style="height: 14rem;">
+            <img class="img-fluid rounded-circle mb-4" src="{{ $foto }}" alt="" style="height: 14rem; width: 14rem;">
             <h5>{{ $nama }}</h5>
             <span>{{ $job }}</span>
         </div>
