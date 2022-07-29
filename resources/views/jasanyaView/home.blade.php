8457664eba0f1792e0c4b8b8e73eea3a397157a6
CROWNIX <rahmatfauzi841@gmail.com>
2022-07-29 18:39:42 +0700

    Menambahkan sedikit shadow pada home

--- resources/views/jasanyaView/home.blade.php
+++ resources/views/jasanyaView/home.blade.php
@@ -46,7 +46,7 @@
     <div class="container py-5 px-lg-5">
         <div class="row g-4">
             <div class="col-lg-4 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="feature-item bg-light rounded text-center p-4">
+                <div class="feature-item bg-light rounded text-center p-4 shadow">
                     <i class="fa fa-3x fa-server text-primary mb-4"></i>
                     <h5 class="mb-3">API</h5>
                     <p class="m-0">Application Programming Interface, cara berinteraksi secara terprogram dengan
@@ -54,7 +54,7 @@
                 </div>
             </div>
             <div class="col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
-                <div class="feature-item bg-light rounded text-center p-4">
+                <div class="feature-item bg-light rounded text-center p-4 shadow">
                     <i class="fa fa-3x fa-mobile text-primary mb-4"></i>
                     <h5 class="mb-3">Aplikasi Mobile</h5>
                     <p class="m-0">Aplikasi mobile (Mobile Apps) yaitu aplikasi yang dibuat untuk perangkat-perangkat
@@ -62,7 +62,7 @@
                 </div>
             </div>
             <div class="col-lg-4 wow fadeInUp" data-wow-delay="0.5s">
-                <div class="feature-item bg-light rounded text-center p-4">
+                <div class="feature-item bg-light rounded text-center p-4 shadow">
                     <i class="fa fa-3x fa-laptop-code text-primary mb-4"></i>
                     <h5 class="mb-3">UI / UX</h5>
                     <p class="m-0">Desain Antarmuka Pengguna, adalah desain yang berfokus pada pengalaman
