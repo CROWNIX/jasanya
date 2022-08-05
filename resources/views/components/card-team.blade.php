9b636cefc17bb2681b8695a547bcbe02a9fa0a81
CROWNIX <rahmatfauzi841@gmail.com>
2022-08-05 20:06:31 +0700

    Menambahkan margin pada setiap card di home dan memperbaiki foto pada halaman team

--- resources/views/components/card-team.blade.php
+++ resources/views/components/card-team.blade.php
@@ -1,18 +1,18 @@
 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-    <div class="shadow hover-scale-1 team-item bg-light rounded">
+    <div class="shadow hover-scale-1 team-item bg-light rounded mb-sm-3 mb-3">
         <div class="text-center border-bottom p-4">
-            <img class="img-fluid rounded-circle mb-4" src="{{ $foto }}" alt="" style="height: 14rem; width: 14rem;">
+            <img class=" rounded-circle mb-4" src="https://source.unsplash.com/gySMaocSdqs/600x1000" alt="" style="height: 14rem; width: 13rem; object-fit:cover; ">
             <h5>{{ $nama }}</h5>
             <span>{{ $job }}</span>
         </div>
         <div class="d-flex justify-content-center p-4">
-            <a class="btn btn-square mx-1 bg-dark" href="{{ $linkGithub }}" target="_blank" ><i
+            <a class="btn shadow btn-square mx-1 bg-dark" href="{{ $linkGithub }}" target="_blank" ><i
                     class="fab fa-github text-white"></i></a>
-            <a class="btn btn-square mx-1 ig-icon" href="{{ $linkIg }}" target="_blank"><i
+            <a class="btn shadow btn-square mx-1 ig-icon" href="{{ $linkIg }}" target="_blank"><i
                     class="fab fa-instagram text-white"></i></a>
-            <a class="btn btn-square mx-1 bg-info" href="{{ $linkLingkedin }}" target="_blank"><i
+            <a class="btn shadow btn-square mx-1 bg-info" href="{{ $linkLingkedin }}" target="_blank"><i
                     class="fab fa-linkedin-in text-white"></i></a>
-            <a class="btn btn-square mx-1 facebook-icon" href="{{ $linkFb }}" target="_blank"><i
+            <a class="btn shadow btn-square mx-1 facebook-icon" href="{{ $linkFb }}" target="_blank"><i
                     class="fab fa-facebook-f text-white"></i></a>
         </div>
     </div>
